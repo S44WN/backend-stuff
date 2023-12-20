81e1fec894b498299fb0e9d37d58c9f661ae748f
Sawan <itissaavn@gmail.com>
2023-12-20 20:58:29 +0530

    Add new stats route; Controllers using aggregate func of mongoDB

--- 4-natours/routes/tourRoutes.js
+++ 4-natours/routes/tourRoutes.js
@@ -8,6 +8,11 @@ router
   .route('/top-tours')
   .get(tourController.aliasTopTours, tourController.getAllTours);
 
+//stats -> aggregate
+router.route('/difficulty').get(tourController.getTourStats);
+
+router.route('/monthly-plan/:year').get(tourController.monthlyPlan);
+
 router
   .route('/')
   .get(tourController.getAllTours)
