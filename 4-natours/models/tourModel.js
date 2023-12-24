e11cd8bfd5bbc900506ffb56944dd69a8586ba6c
Sawan <itissaavn@gmail.com>
2023-12-24 17:42:40 +0530

    Fix eslint errors

--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+// eslint-disable-next-line import/no-extraneous-dependencies
 const slugify = require('slugify');
 // const validator = require('validator');
 
