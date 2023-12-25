91c9defaeaad4e2bd616c5e8b1367d9b89afa5e2
Sawan <itissaavn@gmail.com>
2023-12-25 18:50:17 +0530

    Add DataBase error Handlers

--- 4-natours/controllers/errorController.js
+++ 4-natours/controllers/errorController.js
@@ -1,3 +1,23 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 404);
+};
+
+const handleDuplicateFieldsDB = err => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -9,6 +29,7 @@ const sendErrorDev = (err, res) => {
 
 const sendErrorProd = (err, res) => {
   //Operational, trusted, known errors
+  //   console.log(err)
   if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
@@ -35,6 +56,11 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = { ...err };
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.name === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'VaidationError') error = handleValidationErrorDB(error);
+    sendErrorProd(error, res);
   }
 };
